bf64aa0c012a54de369cf52f91eeda7671573d6f
KASHIF <kashif@Subhans-MacBook-Air.local>
2026-07-30 12:06:32 +0530

    second commit

---
+++ Day 1/index.js
@@ -0,0 +1,20 @@
+const app=require('./app.js');
+// var a=20;
+// var b=20;
+// var c=20;
+// console.log(a+b+c);
+// var x= 20;
+//  if(x==20){
+//     console.log("x is 20");
+//  }else{
+//     console.log("x is not 20");
+//  }
+//  var arr=[1,2,3,4,5];
+//  var i=0;
+//  while( i<=arr.length){
+//     console.log(arr[i]);
+//     i++;
+//  }
+console.log(app.xz);
+console.log(app.yy);
+
